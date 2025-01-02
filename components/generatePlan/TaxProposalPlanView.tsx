cd19797654c6701744df4d97a8ca2218d6efb914
uiabu <ui.abukawsar@gmail.com>
2025-01-02 18:00:55 +0600

    add ViewTaxProposal and ViewTaxPlan components for detailed tax proposal and income breakdown display

--- components/generatePlan/TaxProposalPlanView.tsx
+++ components/generatePlan/TaxProposalPlanView.tsx
@@ -1,10 +1,97 @@
+import Link from "next/link";
 import React from "react";
+import { FaDownload, FaRegEye } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
+import { SiAdobeacrobatreader } from "react-icons/si";
 
 const TaxProposalPlanView = () => {
   return (
-    <div>
-      <h1>Tax Proposal Plan</h1>
-      <p>This is where the tax proposal plan details will be displayed.</p>
+    <div className="flex flex-col gap-10">
+      {/* Plan */}
+      <div>
+        <h2 className="text-4xl font-bold border-b pb-5 mb-6"> Plan</h2>
+        <div className="p-5 2xl:p-8 bg-white rounded-lg shadow-[0px_0px_10px_rgba(0,0,0,0.15)] flex justify-between items-start space-x-4">
+          <div>
+            <div className="flex items-center space-x-3">
+              <div className="p-3 bg-primary text-white rounded-lg ">
+                <SiAdobeacrobatreader className="text-2xl" />
+              </div>
+              <div>
+                <h6 className="text-xl font-medium text-secondary mb-1">
+                  Tax Plan
+                </h6>
+                <p className="text-base font-normal text-secondary opacity-70">
+                  19-06-24s
+                </p>
+              </div>
+            </div>
+            <Link
+              href="#"
+              className="flex items-center space-x-2 mt-3 text-lg justify-start hover:text-primary duration-300 font-medium"
+            >
+              <FaRegEye />
+              <span>View</span>
+            </Link>
+          </div>
+          <div>
+            <Link
+              href="#"
+              className="flex items-center space-x-3 mt-3 text-lg justify-start hover:text-primary duration-300 font-medium"
+              download={true}
+            >
+              <span>Download PDF</span>
+              <FaDownload />
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* Proposal */}
+      <div>
+        <h2 className="text-4xl font-bold border-b pb-5 mb-6"> Proposal</h2>
+        <div className="p-5 2xl:p-8 bg-white rounded-lg shadow-[0px_0px_10px_rgba(0,0,0,0.15)] flex justify-between items-start space-x-4">
+          <div>
+            <div className="flex items-center space-x-3">
+              <div className="p-3 bg-primary text-white rounded-lg ">
+                <SiAdobeacrobatreader className="text-2xl" />
+              </div>
+              <div>
+                <h6 className="text-xl font-medium text-secondary mb-1">
+                  Tax Proposal
+                </h6>
+                <p className="text-base font-normal text-secondary opacity-70">
+                  19-06-24s
+                </p>
+              </div>
+            </div>
+            <div className="flex items-center  gap-5">
+              <Link
+                href="#"
+                className="flex items-center space-x-2 mt-3 text-lg  hover:text-primary duration-300 font-medium"
+              >
+                <IoSend />
+                <span>Send</span>
+              </Link>
+              <Link
+                href="#"
+                className="flex items-center space-x-2 mt-3 text-lg  hover:text-primary duration-300 font-medium"
+              >
+                <FaRegEye />
+                <span>View</span>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <Link
+              href="#"
+              className="flex items-center space-x-3 mt-3 text-lg justify-start hover:text-primary duration-300 font-medium"
+              download={true}
+            >
+              <span>Download PDF</span>
+              <FaDownload />
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ components/generatePlan/ViewTaxProposal.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ViewTaxProposal = () => {
+  return (
+    <div>
+      <div>
+        <h1>TAX PLANNING PROPOSAL</h1>
+        <h4>
+          Proactive plan to reduce or eliminate taxes while complying with
+          federal, state, and local law.
+        </h4>
+        <h6>
+          Review of deductions, legal entity design, retirement, insurance,
+          loopholes, TCJA, FFCRA, CARES, advanced strategies and industry
+          specific strategies.
+        </h6>
+      </div>
+    </div>
+  );
+};
+
+export default ViewTaxProposal;
