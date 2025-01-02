14f6f059df01137222f935cae8cca42e1dfd47fd
uiabu <ui.abukawsar@gmail.com>
2025-01-02 11:52:02 +0600

    enhance tax plan display with detailed income breakdown and estimated federal taxes

--- components/generatePlan/GeneratePlanView.tsx
+++ components/generatePlan/GeneratePlanView.tsx
@@ -343,11 +343,133 @@ const GeneratePlanView = () => {
           </form>
         </div>
         {/* Tax Plan / Right Side */}
-        <div className="w-[70%] border border-secondary p-5">
-          <h2 className="text-2xl font-bold text-secondary text-left mb-2">
-            Tax Plan
-          </h2>
-          <div></div>
+        <div className="w-[70%] ">
+          <div className="border border-[#B1B1B1] p-5 2xl:p-8">
+            <div>
+              <h2 className="text-2xl font-bold text-secondary text-left mb-2">
+                Income Tax Breakdown
+              </h2>
+              <p className="text-xl text-[#555555]">
+                Federal income tax breakdown
+              </p>
+              <h3 className="text-[#B50302] text-4xl font-semibold mt-3">
+                $46,5456
+              </h3>
+            </div>
+            <div className="mt-10 2xl:mt-14 flex flex-col gap-8">
+              <div>
+                <h4 className="text-xl font-semibold text-secondary">
+                  Taxable income
+                </h4>
+                <ul className="space-y-2 py-4 mb-3 border-b">
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      Gross income
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $100,000
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      <span>-</span> Standard deduction
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $13,850
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      <span>-</span> Retirement contributions
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $0
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      <span>-</span> Other deductions
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $0
+                    </span>
+                  </li>
+                </ul>
+                <p className="flex justify-between">
+                  <span className="text-base font-medium text-[#555555]">
+                    Taxable income
+                  </span>
+                  <span className="text-base font-medium text-[#126742]">
+                    $86150
+                  </span>
+                </p>
+              </div>
+              <div>
+                <h4 className="text-xl font-semibold text-secondary">
+                  Estimated federal taxes
+                </h4>
+                <ul className="space-y-2 py-4 mb-3 border-b">
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      Estimated taxes before adjustments
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $14,260
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      <span>-</span> Federal taxes withheld
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $0
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      <span>-</span> Tax credits
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      $0
+                    </span>
+                  </li>
+                </ul>
+                <p className="flex justify-between">
+                  <span className="text-base font-medium text-[#555555]">
+                    Taxes owed
+                  </span>
+                  <span className="text-base font-medium text-[#B50302]">
+                    $14,260.38
+                  </span>
+                </p>
+              </div>
+              <div>
+                <ul className="space-y-2 py-4">
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      Marginal tax rate
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      22%
+                    </span>
+                  </li>
+                  <li className="flex justify-between">
+                    <span className="text-base font-normal text-[#555555]">
+                      Effective tax rate
+                    </span>
+                    <span className="text-base font-normal text-[#126742]">
+                      16.55%
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex items-center  justify-center mt-10 space-x-6  ">
+            <button className="px-4 py-2  text-white rounded-md font-medium text-lg bg-primary hover:bg-hoverColor max-w-3xl hover:text-white text-center w-full">
+              Next
+            </button>
+          </div>
         </div>
       </div>
     </div>
